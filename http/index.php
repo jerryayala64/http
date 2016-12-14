a7aa9a122ae3439cb9fde487e25a62253f86fd57
jerryayala64 <ayalajerry46@gmail.com>
2016-12-14 08:08:38 -0800

    Add files via upload

---
+++ http/index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>delta.ioe </title>
+  <!--  No href external links to css or js-->
+   <link rel="stylesheet" href="css/custom.css">
+	    
+	    <style>
+			
+			#if-location {
+			position: absolute; left: 10px; top:200px;
+			width:800px;
+			height:500px;
+		}
+</style>
+
+</head>
+<body>
+<!--  add more menu items here -->
+<a href="phpcode/" class = "menu" >phpcode</a>&nbsp;
+<a href="downloads/" class = "slink" >downloads</a>&nbsp;
+<a href="logs/" target = "_blank" class = "menu"> logs</a>&nbsp;
+<a href="qnb/" class = "slink" >pma</a>&nbsp;
+<a href="contact.html" target = "_blank" class = "menu" >Contact</a>&nbsp;
+<img src = "img/delta.png" align = "top" height = "28px">
+
+<iframe  id="if-location" name="if" src="intro.html"  scrolling="no" frameborder="3"></iframe>
+
+</body>
+</html>
